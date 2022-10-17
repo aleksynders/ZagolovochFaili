2ccde74b144d2817d53aa411988fd0e629bc3d83
aleksynders <sanyogromov@mail.ru>
2022-10-17 09:15:16 +0300

    Finish

--- ZagolovochFaili/Header.h
+++ ZagolovochFaili/Header.h
@@ -1,8 +1,13 @@
 #pragma once
+
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <malloc.h>
 #include <stdlib.h>
-#define _CRT_SECURE_NO_WARNINGS
+
+char* Soedinenie(char* a, char* b);
+int ConvertToInt(char* s);
+char* ConvertToString(int n);
 
 int Summa(int a, int b)
 {
@@ -12,4 +17,14 @@ int Summa(int a, int b)
 int Raznost(int a, int b)
 {
 	return a - b;
+}
+
+int Proizved(int a, int b)
+{
+	return a * b;
+}
+
+float Chastnoe(int a, int b)
+{
+	return (float)a / (float)b;
 }
--- ZagolovochFaili/Source.c
+++ ZagolovochFaili/Source.c
@@ -7,9 +7,9 @@ int main(void) {
 		int num;
 		while (1 == 1)
 		{
-			printf("Выберите действие: \n1 -> Сумма чисел\n2 -> Разность чисел\n");
+			printf("Выберите действие: \n1 -> Сумма чисел\n2 -> Разность чисел\n3 -> Произведение чисел\n4 -> Частное чисел\n5 -> Объединение двух строк\n6 -> Преобразование числа в строку и строку в число\n7 -> Завершить программу\n");
 			scanf_s("%d", &num);
-			if (num == 1 || num ==2)
+			if (num == 1 || num ==2 || num == 3 || num == 4 || num == 5 || num == 6 || num == 7)
 				break;
 			else {
 				system("CLS");
@@ -40,5 +40,122 @@ int main(void) {
 			system("pause");
 			system("CLS");
 		}
+		if (num == 3)
+		{
+			system("CLS");
+			int a, b;
+			printf("Введите 1 число: ");
+			scanf_s("%d", &a);
+			printf("Введите 2 число: ");
+			scanf_s("%d", &b);
+			printf("Произведение чисел %d и %d = %d \n", a, b, Proizved(a, b));
+			system("pause");
+			system("CLS");
+		}
+		if (num == 4)
+		{
+			system("CLS");
+			int a, b;
+			printf("Введите 1 число: ");
+			scanf_s("%d", &a);
+			printf("Введите 2 число: ");
+			scanf_s("%d", &b);
+			printf("Частное чисел = %.2f \n", Chastnoe(a, b));
+			system("pause");
+			system("CLS");
+		}
+		if (num == 5)
+		{
+			system("CLS");
+			char* a = "строка1 ";
+			char* b = "строка2";
+			printf("%s\n", Soedinenie(a, b));
+			system("pause");
+			system("CLS");
+		}
+		if (num == 6)
+		{
+			system("CLS");
+			char* s = "423412";
+			int num = ConvertToInt(s);
+			printf("Число: %d\n", num);
+			char* str = ConvertToString(num);
+			printf("Строка: %s\n", str);
+			system("pause");
+			system("CLS");
+		}
+		if (num == 7)
+		{
+			break;
+		}
 	}
 }
+
+char* Soedinenie(char* a, char* b)
+{
+	int i = 0;
+	int j = 0;
+	while (a[i] != '\0')
+	{
+		i++;
+	}
+	while (b[j] != '\0')
+	{
+		j++;
+	}
+	char* c = calloc(i + j, sizeof(char));
+	for (int g = 0; g < i; g++)
+	{
+		c[g] = a[g];
+	}
+	for (int g = 0; g < j; g++)
+	{
+		c[g + i] = b[g];
+	}
+	c[i + j] = '\0';
+	return c;
+}
+
+int ConvertToInt(char* a)
+{
+	int num = atof(a);
+	return(num);
+}
+
+char* ConvertToString(int a)
+{
+	int i = 0;
+	int num = a;
+	if (a < 0)
+	{
+		i++;
+	}
+	while (a > 0)
+	{
+		a = a / 10;
+		i++;
+	}
+	char* str = calloc(i, sizeof(char));
+	_itoa(num, str, 10);
+	return(str);
+}
+
+//int Summa(int a, int b)
+//{
+//	return a + b;
+//}
+//
+//int Raznost(int a, int b)
+//{
+//	return a - b;
+//}
+//
+//int Proizved(int a, int b)
+//{
+//	return a * b;
+//}
+//
+//float Chastnoe(int a, int b)
+//{
+//	return (float)a / (float)b;
+//}
